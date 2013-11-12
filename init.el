58053cebbc563df50eab73f8cba9ff701fd8d1e9
Felix11H <Felix11H@imaginary-server.fake>
2013-11-12 08:57:23 +0100

    Working with Latexmk, AucTex

--- init.el
+++ init.el
@@ -27,11 +27,16 @@
     (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 
+
+;;========Personal Gloabl========= 
+
 (tool-bar-mode -1)      ;;no toolbar
 (global-linum-mode t)   ;;global line numbers
 (scroll-bar-mode -1)    ;;no scrollbar
 
-;;Color Themes:
+
+
+;;========Color Themes========= 
 
 (add-to-list 'load-path "~/.emacs.d/color-theme-6.6.0")
 (require 'color-theme)
@@ -42,6 +47,57 @@
 (require 'color-theme-sanityinc-solarized)
 
 
+
+;;============AucTex=========== 
+
+;;(load "auctex.el" nil t t)
+
+(setq TeX-auto-save t)
+;;(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(setq TeX-PDF-mode t)
+
+
+;;============Latexmk=========== 
+
+
+(defun run-latexmk ()
+  (interactive)
+  (let ((TeX-save-query nil)
+        (TeX-process-asynchronous nil)
+        (master-file (TeX-master-file)))
+    (TeX-save-document "")
+    (TeX-run-TeX "latexmk"
+                 (TeX-command-expand "latexmk %t" 'TeX-master-file)
+                 master-file)
+    (if (plist-get TeX-error-report-switches (intern master-file))
+        (TeX-next-error t)
+      (minibuffer-message "latexmk done"))))
+
+(add-hook 'LaTeX-mode-hook
+          (lambda () (local-set-key (kbd "C-0") #'run-latexmk)))
+
+
+;;from http://stackoverflow.com/questions/7860547/is-there-a-way-to-prevent-font-locking-from-changing-the-font-family-and-change 
+;; Only change sectioning colour
+(setq font-latex-fontify-sectioning 'color)
+;; super-/sub-script on baseline
+(setq font-latex-script-display (quote (nil)))
+;; Do not change super-/sub-script font
+(custom-set-faces
+ '(font-latex-subscript-face ((t nil)))
+ '(font-latex-superscript-face ((t nil)))
+ )
+;; Exclude bold/italic from keywords
+(setq font-latex-deactivated-keyword-classes
+    '("italic-command" "bold-command" "italic-declaration" "bold-declaration"))
+
+
+
+
+
+
+;;Defunct:
 
 ;;(defun python-mode-theme-hook ()
   ;;(color-theme-initialize)
@@ -54,6 +110,8 @@
 ;;(require 'color-theme-solarized)
 
 ;;(add-to-list 'custom-theme-load-path "~/.emacs.d/solarized-emacs/")
+
+
 
 
 
